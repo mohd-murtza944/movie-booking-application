2a3d5b89d021664d4619191e55914637ac2c7da3
Mohd Murtza Choudhary <mohdmurtzachoudhary@gmail.com>
2023-12-19 18:25:03 +0530

    final commit

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const userRouter = express.Router();
+
+const { getAllUser, signUp, updateUser, deleteUser, logIn, getBookingofUser, getUserById } = require('../controllers/user-controller');
+
+userRouter.get("/", getAllUser);
+userRouter.post("/signup", signUp)
+userRouter.put("/:id", updateUser)
+userRouter.delete("/:id", deleteUser)
+userRouter.post("/login", logIn)
+userRouter.get("/bookings/:id", getBookingofUser);
+userRouter.get("/:id", getUserById);
+
+module.exports = userRouter 
+
+
+
